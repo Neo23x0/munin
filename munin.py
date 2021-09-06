c79f2d04b3ae7a5d889acc3e32771763a596fb75
Oleksiy Gayda <78700050+0x4f47@users.noreply.github.com>
2021-09-06 12:38:05 -0700

    Update munin.py to fix minor typo
    Updated to fix a minor typo in word "Response".

--- munin.py
+++ munin.py
@@ -678,7 +678,7 @@ def getTotalHashInfo(sha1):
         if args.debug:
             print("[D] Querying Totalhash: %s" % preparedURL)
         response = requests.get(preparedURL, proxies=connections.PROXY)
-        # print "Respone: '%s'" % response.content
+        # print "Response: '%s'" % response.content
         if response.content and \
                         '0 of 0 results' not in response.content and \
                         'Sorry something went wrong' not in response.content:
@@ -710,7 +710,7 @@ def getURLhaus(md5, sha256):
         else:
             data = {"md5_hash": md5}
         response = requests.post(URL_HAUS_URL, data=data, timeout=3, proxies=connections.PROXY)
-        print("Respone: '%s'" % response.json())
+        print("Response: '%s'" % response.json())
         res = response.json()
         if res['query_status'] == "ok" and res['md5_hash']:
             info['urlhaus_available'] = True
